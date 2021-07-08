783b06fb36ae3be8731b50c3c887d5a31852d33a
kyoungkyoung <wkjang4@gmail.com>
2021-07-08 13:24:03 +0900

    etc table 수정

--- adminPage/src/main/java/chart/bean/EtcDTO.java
+++ adminPage/src/main/java/chart/bean/EtcDTO.java
@@ -1,11 +1,16 @@
 package chart.bean;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class EtcDTO {
 	private int seq;
 	private String etcLogtime;
+	private String email;
 	private int hitHit; 			//조회수
 	private int likeLike;			//좋아요수
 	private int shareShare;			//공유수
